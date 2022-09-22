85a0dc8cbbe703919729e86189d5550d635b93e4
Kaijie Chen <ckj@apache.org>
2022-09-22 17:44:15 +0800

    grant lease for CLOSING containers

--- hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/BlockOutputStreamEntryPool.java
+++ hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/BlockOutputStreamEntryPool.java
@@ -251,6 +251,7 @@ public class BlockOutputStreamEntryPool {
     // FIXME: find a more proper next renew time.
     nextContainerLeaseRenew.set(result.getRight() - 10);
 
+    // TODO: it is possible that some containers are not renewed.
     for (BlockOutputStreamEntry entry : streamEntries) {
       if (!entry.isClosed() &&
           !renewed.contains(entry.getBlockID().getContainerID())) {
